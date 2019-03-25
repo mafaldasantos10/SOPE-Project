e6c1fefc25da08fed6ab7ac921433798aa9bac53
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-03-25 13:43:30 +0000

    Started changes to output structure

--- forensic.c
+++ forensic.c
@@ -70,23 +70,26 @@ void init(int argc, char *argv[])
 }
 
 /** @brief returns a string with the date complying with the ISO 8601 format, <date>T<time> */
-char *formatDate(char *s, time_t val)
+char *formatDate(char s[], time_t val)
 {
-    strftime(s, 50, "%Y-%m-%dT%X", localtime(&val));
+    strftime(s, 20, "%Y-%m-%dT%X", localtime(&val));
     return s;
 }
 
-char *formatPerm(char *s, int mode)
+char *formatPerm(char s[], int mode)
 {
     char str[4];
     int i = -1;
-    if (mode & S_IRUSR){    //User has reading permission
+    if (mode & S_IRUSR)
+    { //User has reading permission
         str[++i] = 'r';
     }
-    if (mode & S_IWUSR){    //User has writing permission
+    if (mode & S_IWUSR)
+    { //User has writing permission
         str[++i] = 'w';
     }
-    if (mode & S_IXUSR){   //User has executing permission
+    if (mode & S_IXUSR)
+    { //User has executing permission
         str[++i] = 'x';
     }
 
@@ -95,14 +98,17 @@ char *formatPerm(char *s, int mode)
     return strcpy(s, str);
 }
 
-/** @brief runs a given shell command in the format "command filename" */
-void runCommand(char command[], char filename[])
+/** @brief runs a given shell command in the format "command filename". It's output is sent to out_buffer
+ * 
+ *  @param command String containing the command name
+ *  @param filename String containing the name of the file
+ *  @param out_buffer String to whom the output will be sent
+ * 
+ *  @return Number of chars written to out_buffer
+ */
+int runCommand(char command[], char filename[], char out_buffer[])
 {
-
-    char buffer[BUFFER_SIZE];
-    char *token;
-    int pipe_fd[2];             //pipe used for redirecting the output of the command
-    int out_fd = STDOUT_FILENO; //By default, the output will go to stdout
+    int pipe_fd[2]; //pipe used for redirecting the output of the command
     pid_t pid;
 
     /* setting the communication pipe */
@@ -133,31 +139,33 @@ void runCommand(char command[], char filename[])
         exit(2);
     }
 
-    /* prints the command output */
-    read(pipe_fd[READ], buffer, BUFFER_SIZE);
-    if (!strcmp(command, "file"))
-    {
-        /* printing first two fields of file command*/
-        token = strtok(buffer, ":");
-        write(out_fd, token, strlen(token));
-        token = strtok(NULL, ",\n");
-        token[0] = ','; //swapping the first space in the 2nd field of file's output for a colon
-        write(out_fd, token, strlen(token));
-    }
-    else
-    {
-        token = strtok(buffer, " "); /* only selects the argument before the ' ' (space), i.e. the actual cripto */
-        write(out_fd, token, strlen(token));
-    }
+    /* sends the command output to out_buffer */
+    return read(pipe_fd[READ], out_buffer, BUFFER_SIZE);
+    // if (!strcmp(command, "file"))
+    // {
+    //     /* printing first two fields of file command*/
+    //     token = strtok(buffer, ":");
+    //     write(out_fd, token, strlen(token));
+    //     token = strtok(NULL, ",\n");
+    //     token[0] = ','; //swapping the first space in the 2nd field of file's output for a colon
+    //     write(out_fd, token, strlen(token));
+    // }
+    // else
+    // {
+    //     token = strtok(buffer, " "); /* only selects the argument before the ' ' (space), i.e. the actual cripto */
+    //     write(out_fd, token, strlen(token));
+    // }
 }
 
 /** @brief prints the stats of a file */
 void printStats(struct stat fileStat, char filename[])
 {
+    char date[20];
+    char buffer[BUFFER_SIZE];
+    char* token;
+    int out_fd = STDOUT_FILENO; // out_fd is the file descriptor of the output. By default it is stdout fd
 
-    char date[50];
-
-    runCommand("file", filename);                       /* file name and type */
+    runCommand("file", filename, buffer);               /* file name and type */
     printf(",%lu", fileStat.st_size);                   /* file size */
     printf(",%s", formatPerm(date, fileStat.st_mode));  /* file protection */
     printf(",%s", formatDate(date, fileStat.st_atime)); /* time of last access */
@@ -166,15 +174,15 @@ void printStats(struct stat fileStat, char filename[])
     /* md5 */
     //If bit is set, numerical value will be greater than 0 -> true
     if (mask & 0x10)
-        runCommand("md5sum", filename);
+        runCommand("md5sum", filename, buffer);
 
     /* sha1 */
     if (mask & 0x08)
-        runCommand("sha1sum", filename);
+        runCommand("sha1sum", filename, buffer);
 
     /* sha256 */
     if (mask & 0x04)
-        runCommand("sha256sum", filename);
+        runCommand("sha256sum", filename, buffer);
 
     printf("\n");
 }
