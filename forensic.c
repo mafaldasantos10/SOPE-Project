4703bb7dd4bd6063ea552ee477046e1f61df234f
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-03-27 10:07:24 +0000

    Added documentation for every class. Implemented recursivity (no child processes)

--- forensic.c
+++ forensic.c
@@ -9,16 +9,20 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <time.h>
-#include "utility.h"
 #include <dirent.h>
+#include "utility.h"
 
 #define READ 0
 #define WRITE 1
 #define BUFFER_SIZE 512
 
-Mode mode;
+opt_t options;
 
-/** @brief ... */
+/** @brief Initializes the program. Checks for invalid inputs and parses exection options (setting opt_t fields to true)
+ * 
+ *  @param argc Number of arguments (directly from main)
+ *  @param argv Array containing the arguments (directly from main)
+*/
 void init(int argc, char *argv[])
 {
     char *token;
@@ -32,7 +36,7 @@ void init(int argc, char *argv[])
     for (int i = 1; i < argc - 1; i++)
     {
         if (!strcmp(argv[i], "-r"))
-            mode.r = 1;
+            options.r = 1;
 
         if (!strcmp(argv[i - 1], "-h"))
         {
@@ -42,13 +46,13 @@ void init(int argc, char *argv[])
             {
 
                 if (!strcmp(token, "md5"))
-                    mode.md5 = 1;
+                    options.md5 = 1;
 
                 if (!strcmp(token, "sha1"))
-                    mode.sha1 = 1;
+                    options.sha1 = 1;
 
                 if (!strcmp(token, "sha256"))
-                    mode.sha256 = 1;
+                    options.sha256 = 1;
 
                 token = strtok(NULL, ",");
             }
@@ -63,34 +67,47 @@ void init(int argc, char *argv[])
                 exit(1);
             }
             dup2(out, STDOUT_FILENO);
-            mode.o = 1;
+            options.o = 1;
         }
 
         if (!strcmp(argv[i], "-v"))
-            mode.v = 1;
+            options.v = 1;
     }
 }
 
-/** @brief returns a string with the date complying with the ISO 8601 format, <date>T<time> */
+/** @brief Formats the value to a date complying with the ISO 8601 format, <date>T<time>
+ * 
+ *  @param s String (char array) to where the output will be written
+ *  @param val Time value as in struct stat st_?time fields
+ * 
+ *  @return Reference to string s
+*/
 char *formatDate(char s[], time_t val)
 {
     strftime(s, 20, "%Y-%m-%dT%X", localtime(&val));
     return s;
 }
 
-char *formatPerm(char s[], int mode)
+/** @brief Formats the file permissions value into a string containing the user permissions.
+ *  
+ *  @param s String (char array) to where the output is written
+ *  @param st_mode Struct stat st_mode field value (holds the file permissions)
+ * 
+ *  @return Reference to string s
+ */
+char *formatPermissions(char s[], int st_mode)
 {
     char str[4];
     int i = -1;
-    if (mode & S_IRUSR)
+    if (st_mode & S_IRUSR)
     { //User has reading permission
         str[++i] = 'r';
     }
-    if (mode & S_IWUSR)
+    if (st_mode & S_IWUSR)
     { //User has writing permission
         str[++i] = 'w';
     }
-    if (mode & S_IXUSR)
+    if (st_mode & S_IXUSR)
     { //User has executing permission
         str[++i] = 'x';
     }
@@ -100,33 +117,31 @@ char *formatPerm(char s[], int mode)
     return strcpy(s, str);
 }
 
-/** @brief runs a given shell command in the format "command filename". It's output is sent to out_buffer
+/** @brief Runs a given shell command in the format "command filename". It's output is sent to out_buffer
  * 
- *  @param command String containing the command name
- *  @param filename String containing the name of the file
- *  @param out_buffer String to whom the output will be sent
+ *  @param command Command name
+ *  @param filename File name
+ *  @param out_buffer String to where the output will be sent
  * 
  *  @return Number of chars written to out_buffer
  */
 int runCommand(char command[], char filename[], char out_buffer[])
 {
-    int pipe_fd[2]; //pipe used for redirecting the output of the command
+    int pipe_fd[2];
     pid_t pid;
 
-    /* setting the communication pipe */
     if (pipe(pipe_fd) < 0)
     {
         perror("runCommand: pipe");
         exit(1);
     }
 
-    /* runs the command */
     pid = fork();
 
     if (pid != 0) // PARENT
     {
         close(pipe_fd[WRITE]);
-        wait(NULL); /* wait for the child to terminate */
+        wait(NULL);
     }
     else if (pid == 0) // CHILD
     {
@@ -141,57 +156,65 @@ int runCommand(char command[], char filename[], char out_buffer[])
         exit(2);
     }
 
-    /* sends the command output to out_buffer */
     return read(pipe_fd[READ], out_buffer, BUFFER_SIZE);
 }
 
+/** @brief Prints the first two fields of 'file' command output
+ * 
+ *  @param buffer String (char array) holding 'file's output.
+ */
 void printFileCmd(char buffer[])
 {
     char *token;
 
-    /* printing first two fields of file command*/
     token = strtok(buffer, ":");
     printf("%s", token);
     token = strtok(NULL, ",\n");
-    printf(",%s", ++token); //skipping the first char (space)
+    //token is incremented to avoid printing a space character after the comma
+    printf(",%s", ++token);
 }
 
+/** @brief Prints the criptograghic summary resulting of a command (either md5sum,sha1sum or sha256sum)
+ * 
+ *  @param buffer String (char array) holding the command output.
+ */
 void printSum(char buffer[])
 {
     char *token;
 
-    token = strtok(buffer, " "); /* only selects the argument before the ' ' (space), i.e. the actual cripto */
+    token = strtok(buffer, " ");
     printf(",%s", token);
 }
 
-/** @brief prints the stats of a file */
+/** @brief Prints the stats of a file, according the inputed options.
+ * 
+ *  @param fileStat Struct stat variable with the file properties
+ *  @param filename Name of the file whose stats will be printed
+ */
 void printStats(struct stat fileStat, char filename[])
 {
     char str[BUFFER_SIZE];
 
-    runCommand("file", filename, str); /* file name and type */
+    runCommand("file", filename, str);
     printFileCmd(str);
-    printf(",%lu", fileStat.st_size);                  /* printing file size */
-    printf(",%s", formatPerm(str, fileStat.st_mode));  /* printing file protection */
-    printf(",%s", formatDate(str, fileStat.st_atime)); /* printing time of last access */
-    printf(",%s", formatDate(str, fileStat.st_mtime)); /* printing time of last modification */
+    printf(",%lu", fileStat.st_size);
+    printf(",%s", formatPermissions(str, fileStat.st_mode));
+    printf(",%s", formatDate(str, fileStat.st_atime));
+    printf(",%s", formatDate(str, fileStat.st_mtime));
 
-    /* md5 */
-    if (mode.md5)
+    if (options.md5)
     {
         runCommand("md5sum", filename, str);
         printSum(str);
     }
 
-    /* sha1 */
-    if (mode.sha1)
+    if (options.sha1)
     {
         runCommand("sha1sum", filename, str);
         printSum(str);
     }
 
-    /* sha256 */
-    if (mode.sha256)
+    if (options.sha256)
     {
         runCommand("sha256sum", filename, str);
         printSum(str);
@@ -200,6 +223,20 @@ void printStats(struct stat fileStat, char filename[])
     printf("\n");
 }
 
+/** @brief Checks if a directory is either '.' or '..'
+ * 
+ *  @param directory Directory name
+ * 
+ *  @return True if it isn't '.' nor '..', False otherwise
+ */
+int notPoint(const char *directory){
+    return strcmp(".", directory) && strcmp("..", directory);
+}
+
+/** @brief Analyses the directory and its subdirectories to get every file stats.
+ * 
+ *  @param directory Directory name
+ */
 void readDirectory(char *directory)
 {
     DIR *dir;
@@ -207,9 +244,9 @@ void readDirectory(char *directory)
     struct stat fileStat;
     char filePath[258];
 
-    if (stat(directory, &fileStat))
+    if (lstat(directory, &fileStat))
     {
-        perror("Stat");
+        perror(directory);
         exit(1);
     }
 
@@ -225,22 +262,20 @@ void readDirectory(char *directory)
         {
             sprintf(filePath, "%s/%s", directory, dirent->d_name);
 
-            //printf("%s\n", filePath);
-
-            if (stat(filePath, &fileStat) == -1)
+            if (lstat(filePath, &fileStat) == -1)
             {
-                perror("readDirectory");
+                perror(directory);
                 exit(3);
             }
 
             if (S_ISDIR(fileStat.st_mode))
             {
-                continue;
+                if(notPoint(dirent->d_name) && options.r)
+                    readDirectory(filePath);
             }
             else
             {
                 printStats(fileStat, filePath);
-                //perror("After printStats");
             }
         }
     }
