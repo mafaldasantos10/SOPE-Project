a8be499b2472cec37b021356dc78e8650b5df5dc
mafaldasantos10 <anamaf13@gmail.com>
2019-05-12 18:52:44 +0100

    userFifo, filling structs, documentation and code restructuring

--- Part II/utility.h
+++ Part II/utility.h
@@ -1,11 +1,64 @@
 #pragma once
 
+/*
+ * ================================================================================================
+ * USER Related Utility
+ * ================================================================================================
+ */
+
+/**
+ * @brief Verifies if the indicated accountID follows the correct nomenclature
+ * 
+ * @param char *accountID Given AccountID
+ */
 void validateAccountID(char *accountID);
 
+/**
+ * @brief Verifies if the indicated balance is valid
+ * 
+ * @param char *balance Given balance
+ */
 void validateBalance(char *balance);
 
+/**
+ * @brief Verifies if the indicated password follows the correct nomenclature
+ * 
+ * @param char *pass Given password
+ */
 void validatePassword(char *pass);
 
+/**
+ * @brief Verifies if the indicated amount is valid
+ * 
+ * @param char* amount Given Amount
+ */
 void validateAmount(char *amount);
 
+/**
+ * @brief Verifies if the indicated delay is valid
+ * 
+ * @param char* delay Given Delay
+ */
 void validateDelay(char *delay);
+
+/*
+ * ================================================================================================
+ * SERVER Related Utility
+ * ================================================================================================
+ */
+
+/**
+ * @brief Generates a random set of hexadecimal numbers (salt)
+ * 
+ * @return char *salt Salt
+ */
+char *generateSalt();
+
+/**
+ * @brief Generates a encription made with the salt and password
+ * 
+ * @param char *salt Used for the encription
+ * @param char *password Used for the encription
+ * @return char *encripted Generated hash
+ */
+char *generateHash(char *salt, char *password);
